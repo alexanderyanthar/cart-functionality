e60cde25047acc1086392b77498264005271413c
alexanderyanthar <alexander.yanthar@outlook.com>
2023-06-05 22:21:51 -0400

    created basic index, styles and functionality for a cart with firebase realtime database

---
+++ app.js
@@ -0,0 +1,152 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  import { getDatabase, ref, update, onValue, get } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBEyiSZmKmPC4MR9akcYXvevnR874ZSEqk",
+    authDomain: "test-thingy-83c89.firebaseapp.com",
+    databaseURL: "https://test-thingy-83c89-default-rtdb.firebaseio.com",
+    projectId: "test-thingy-83c89",
+    storageBucket: "test-thingy-83c89.appspot.com",
+    messagingSenderId: "30294068563",
+    appId: "1:30294068563:web:951b0dae48f387baec25a6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  const itemsRef = ref(database);
+
+  const itemStore = document.querySelector('.itemStore');
+  const cart = document.querySelector('.cart');
+
+  const itemStoreItems = [];
+  const cartItems = [];
+
+  cart.addEventListener('click', function(e) {
+    updateCart(e, false);
+  })
+
+  itemStore.addEventListener('click', function(e) {
+    updateCart(e, true);
+  })
+
+  function updateCart(e, addToCart) {
+    if (e.target.tagName === 'BUTTON') {
+      const id = e.target.id.slice(1);
+      const itemId = `item${id}`;
+      const itemRef = ref(database, `/items/${itemId}`);
+      
+      // Get the current quantity value for the item
+      get(itemRef).then((snapshot) => {
+        const itemData = snapshot.val();
+        console.log(itemData);
+        
+        // Increment or decrement the quantity value and update the inCart flag
+        const newQuantity = (itemData.quantity || 0) + (addToCart ? 1 : -1);
+        const updatedData = {
+          quantity: newQuantity,
+          inCart: newQuantity > 0
+        };
+        
+        // Update the item with the new quantity and inCart flag
+        update(itemRef, updatedData)
+          .then(() => {
+            console.log('Item updated');
+          })
+          .catch((err) => {
+            console.error('Failed to update item', err);
+          });
+      });
+    }
+  }
+
+
+  onValue(itemsRef, (data) => {
+    const allItems = [];
+
+    if (data.exists()) {
+      const payload = data.val().items;
+      console.log(payload);
+      for (let item in payload) {
+        allItems.push(payload[item]);
+      }
+
+      cartItems.length = 0;
+
+      allItems.forEach((item) => {
+        if (item.inCart) {
+          cartItems.push(item);
+        }
+      });
+
+      displayItems(allItems, itemStore);
+      displayItemsWithQuantity(cartItems, cart);
+    } else {
+      console.log('no data');
+    }
+  });
+
+
+  const displayItems = (arrOfItems, node) => {
+    node.innerHTML = '';
+
+    arrOfItems.forEach((item) => {
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+      const h3 = document.createElement('h3');
+      const span = document.createElement('span');
+      const button = document.createElement('button');
+
+      button.id = item.id;
+      h3.textContent = item.name;
+      span.textContent = `$${item.price}`;
+      button.textContent = 'Add to cart';
+
+      
+
+      img.alt = item.name;
+      img.src = item.img;
+      li.append(h3);
+      li.append(button);
+      li.append(img);
+      li.append(span);
+      node.append(li);
+
+    })
+  }
+
+  const displayItemsWithQuantity = (arrOfItems, node) => {
+    node.innerHTML = '';
+
+    arrOfItems.forEach((item) => {
+      if (item.quantity > 0) {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        const h3 = document.createElement('h3');
+        const span = document.createElement('span');
+        const button = document.createElement('button');
+        const quantity = document.createElement('span');
+
+        button.id = item.id;
+        h3.textContent = item.name;
+        span.textContent = `$${item.price}`;
+        button.textContent = 'Remove';
+
+        quantity.textContent = item.quantity || 0;
+        li.append(quantity);
+
+        img.alt = item.name;
+        img.src = item.img;
+        li.append(h3);
+        li.append(button);
+        li.append(img);
+        li.append(span);
+        li.append(quantity);
+        node.append(li);
+      }
+    });
+  }
